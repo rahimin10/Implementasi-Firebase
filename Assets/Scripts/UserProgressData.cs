3c69f3927199a291dbcc7112222afdf8a826f698
rahimin10 <rahimin710@gmail.com>
2021-10-22 22:51:11 +0700

    push step1

---
+++ Assets/Scripts/UserProgressData.cs
@@ -0,0 +1,14 @@
+﻿//Script UserProgressData adalah sebagai object untuk menyimpan progress user
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class UserProgressData
+{
+	public double Gold = 0;
+	public List<int> ResourcesLevels = new List<int> ();
+}
+
